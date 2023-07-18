de90599d6f49bc0fab577bae8e692ac0329590bf
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-18 15:25:54 -0400

    Customizing HTTP Requests

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const request = require('request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=325d5f8462a2e853d9ba2d344336725f&query=37.8267,-122.4233';
+  'http://api.weatherstack.com/current?access_key=325d5f8462a2e853d9ba2d344336725f&query=37.8267,-122.4233&units=f';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  // console.log(response.body.current);
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      '. It is currently ' +
+      response.body.current.temperature +
+      ' degrees out. It feels like ' +
+      response.body.current.feelslike +
+      ' degrees out.'
+  );
 });
