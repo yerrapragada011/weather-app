96b722d6e40f2a98bf90db0e7cc70c9b01f1e185
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-20 14:20:40 -0400

    Destructuring and Property Shorthand Challenge

--- utils/forecast.js
+++ utils/forecast.js
@@ -8,19 +8,19 @@ const forecast = (latitude, longitude, callback) => {
     longitude +
     '&units=f';
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service! ', undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
       callback(
         undefined,
-        response.body.current.weather_descriptions[0] +
+        body.current.weather_descriptions[0] +
           '. It is currently ' +
-          response.body.current.temperature +
+          body.current.temperature +
           ' degrees out. It feels like ' +
-          response.body.current.feelslike +
+          body.current.feelslike +
           ' degrees out.'
       );
     }
